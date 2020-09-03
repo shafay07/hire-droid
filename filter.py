590e00d135d1ffdcb55db928961bd675fff37432
SHAFAY HASEEB <32669422+shafay07@users.noreply.github.com>
2020-09-04 01:01:36 +0300

    added results.csv functionality

--- filter.py
+++ filter.py
@@ -41,16 +41,21 @@ def getPdfBookmarks(batch_file):
 
 
 # extracts the text for each resume
-def getEachResumeText(current_batch, pdf_reader, bookmarks, start_bookmark=0):
+def getEachResumeText(current_batch, pdf_reader, bookmarks, bookmarks_len, start_bookmark=0):
     start_page = pdf_reader.getDestinationPageNumber(
         bookmarks[start_bookmark])
-    end_bookmark = start_bookmark+1
-    end_page = pdf_reader.getDestinationPageNumber(
-        bookmarks[end_bookmark])
+    # at last bookmark set end_page to last page of the document
+    if bookmarks_len == start_bookmark:
+        end_page = pdf_reader.getNumPages()
+    # else set it to next bookmark
+    else:
+        end_bookmark = start_bookmark+1
+        end_page = pdf_reader.getDestinationPageNumber(
+            bookmarks[end_bookmark])
 
     # itterate over a complete resume to extract text
+    clean_extracted = ''
     for page in range(start_page, end_page):
-        clean_extracted = ''
         with pdfplumber.open(current_batch) as pdf:
             current_page = pdf.pages[page]
             extracted = current_page.extract_text()
@@ -59,53 +64,45 @@ def getEachResumeText(current_batch, pdf_reader, bookmarks, start_bookmark=0):
             clean_extracted += cleaned
     # helper function to save whats extracted by pdfplumber in a text file
     saveCleanText(clean_extracted)
-
-    return clean_extracted
-    #     while count < num_pages:
-    #         pageObj = pdfReader.getPage(count)
-    #         count += 1
-    #         text += pageObj.extractText()
-
-    #     # Convert all strings to lowercase
-    #     text = text.lower()
-
-    #     # Remove numbers
-    #     text = re.sub(r'\d+', '', text)
-
-    #     # Remove punctuation
-    #     text = text.translate(str.maketrans('', '', string.punctuation))
-    # #########################################################################
-
-    #     y = 0
-    #     # Obtain the scores for each area
-    #     for k, v in terms.items():
-    #         for word in v:
-    #             if word in text:
-    #                 y += 1
-    #         candidates[k].append(y)
-    #         y = 0
-    #     candidates['Name'].append('=HYPERLINK("{}", "{}")'.format(
-    #         os.path.join('extracted_cvs', os.path.basename(x)), os.path.basename(x)))
-    #     candidates['Text'].append(text)
-
-    # print('Done...')
-    # print('Generating output file...')
-    # # Create a dataframe for the summary table
-    # summary = pd.DataFrame(candidates)
-    # summary.set_index('Name', inplace=True)
-    # # Export summary table
-    # summary.to_csv('results.csv')
+    return clean_extracted, start_page
 
 
 # Itterate over each resume for each batch to search the matching keywords
 def searchEachResume(batch_pdf, keywords):
+    # set csv header for results.csv
+    setCsvHeader(keywords.keys())
+    # for each batch
     for current_batch in batch_pdf:
         pdfReader, bookmarks = getPdfBookmarks(current_batch)
+        bookmarks_len = getBookmarkLength(bookmarks)
+        # for each resume in a batch
         for resume_index, resume in enumerate(tqdm(bookmarks, bar_format='{l_bar}{bar:50}{r_bar}{bar:-10b}')):
-            clean_extracted = getEachResumeText(
-                current_batch, pdfReader, bookmarks, resume_index)
-            if resume_index == 5:
-                exit()
+            # for resume_index, resume in enumerate(tqdm(range(96, 98), bar_format='{l_bar}{bar:50}{r_bar}{bar:-10b}')):
+            clean_extracted, start_page = getEachResumeText(
+                current_batch, pdfReader, bookmarks, bookmarks_len, resume_index)
+            # get hits for each keyword
+            # create a hit list
+            hits = {}
+            for key, value in keywords.items():
+                for val in value:
+                    count = clean_extracted.count(val)
+                    hits["Applicant id"] = '=HYPERLINK("{}#page={}","{}")'.format(
+                        current_batch, start_page+1, resume_index+1)
+                    hits[key] = count
+            saveCsvResults(hits)
+
+
+# Helper function to get total number of pages
+def getBookmarkLength(bookmarks):
+    return len(bookmarks)-1
+
+
+# Helper function to set header for csv file
+def setCsvHeader(header):
+    list_header = list(header)
+    list_header.insert(0, 'Applicant ID')
+    df = pd.DataFrame(columns=list_header)
+    df.to_csv('results.csv', index=False)
 
 
 # Helper function to write a text file to analyze what is extracted
@@ -113,14 +110,14 @@ def saveCleanText(to_write):
     file1 = open("clean_extracted.txt", "a",
                  encoding='utf-8')  # append mode
     file1.write("\n"+to_write+"\n")
-
     file1.write("***NEXT RESUME***")
     file1.close()
 
 
 # Save final results of keywords matched in a csv
-def saveCsvResults():
-    pass
+def saveCsvResults(to_append):
+    df = pd.DataFrame(to_append, index=[0])
+    df.to_csv('results.csv', mode='a', header=False, index=False)
 
 
 if __name__ == "__main__":
@@ -129,4 +126,4 @@ if __name__ == "__main__":
     batchPdf, keywords = loadFiles()
     searchEachResume(batchPdf, keywords)
     print('Done...')
-    print('Please find results.csv file, thank you for using Hire-Droid.')
+    print('\nPlease find results.csv file, thank you for using Hire-Droid.')
